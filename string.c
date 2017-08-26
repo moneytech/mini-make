791b115ab03226ff6ff7f122637ded540b4a7add
Taylor Holberton <taylorcholberton@gmail.com>
2017-08-26 15:16:05 -0700

    implemented make_string_copy()

--- string.c
+++ string.c
@@ -1,6 +1,21 @@
 #include <make/string.h>
 
+#include <errno.h>
+#include <stdlib.h>
 #include <string.h>
+
+int make_string_copy(const struct make_string *src,
+                     struct make_string *dst) {
+  /* allocate an extra character for
+   * a null-terminator */
+  dst->data = malloc(src->size + 1);
+  if (dst->data == NULL)
+    return -ENOMEM;
+  memcpy(dst->data, src->data, src->size);
+  dst->data[src->size] = 0;
+  dst->size = src->size;
+  return 0;
+}
 
 int make_string_equal(const struct make_string *a,
                       const struct make_string *b) {
