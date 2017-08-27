34eca4ba2b7c882bcffc4de13ab7b6d5daa18f7c
Taylor Holberton <taylorcholberton@gmail.com>
2017-08-27 07:52:52 -0700

    improved command line interface

--- make.c
+++ make.c
@@ -6,10 +6,28 @@
 #include <make/string.h>
 #include <make/table.h>
 
+#include <limits.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <string.h>
+
+char *argv0 = "mini-make";
+
+static void print_help(void) {
+  printf("Usage: %s [options] [target] ...\n", argv0);
+  printf("\n");
+  printf("Options:\n");
+  printf("  -f, --file <FILE>     : Read FILE instead of default 'Makefile'.\n");
+  printf("  -C, --directory <DIR> : Change directory to DIR before running makefile.\n");
+  printf("  -j, --jobs <N>        : Use up to 'N' number of jobs.\n");
+  printf("  -h, --help            : Print this help message.\n");
+  printf("  -v, --version         : Print version information.\n");
+}
+
+static void print_version(void) {
+  printf("Mini Make 0.1\n");
+}
 
 int main(int argc, char **argv) {
 
@@ -20,6 +38,56 @@ int main(int argc, char **argv) {
   struct make_options options;
   struct make_string make_key;
   struct make_string make_value;
+
+  argv0 = argv[0];
+
+  options.filename = "Makefile";
+  options.working_dir = ".";
+  options.jobs = 1;
+
+  for (i = 1; i < argc; i++) {
+    if ((strcmp(argv[i], "-C") == 0)
+     || (strcmp(argv[i], "--directory") == 0)) {
+      options.working_dir = argv[i + 1];
+      if (options.filename == NULL) {
+        fprintf(stderr, "No directory given for '%s'\n", argv[i]);
+        return EXIT_FAILURE;
+      }
+      i++;
+    } else if ((strcmp(argv[i], "-f") == 0)
+            || (strcmp(argv[i], "--file") == 0)
+            || (strcmp(argv[i], "--makefile") == 0)) {
+      options.filename = argv[i + 1];
+      if (options.filename == NULL) {
+        fprintf(stderr, "No file name given for '%s'\n", argv[i]);
+        return EXIT_FAILURE;
+      }
+      i++;
+    } else if ((strcmp(argv[i], "-j") == 0)
+            || (strcmp(argv[i], "--jobs") == 0)) {
+      if (i + 1 >= argc) {
+          options.jobs = INT_MAX;
+      } else if (sscanf(argv[i + 1], "%u", &options.jobs) != 0) {
+        fprintf(stderr, "Incorrect numerical value given for '%s'\n", argv[i]);
+        return EXIT_FAILURE;
+      }
+    } else if ((strcmp(argv[i], "-h") == 0)
+            || (strcmp(argv[i], "--help") == 0)) {
+      print_help();
+      return EXIT_FAILURE;
+    } else if ((strcmp(argv[i], "-v") == 0)
+            || (strcmp(argv[i], "--version") == 0)) {
+      print_version();
+      return EXIT_FAILURE;
+    } else if (argv[i][0] != '-') {
+      /* The non-options will be
+       * parsed in the next loop */
+      break;
+    } else {
+      fprintf(stderr, "Unknown option '%s'\n", argv[i]);
+      return EXIT_FAILURE;
+    }
+  }
 
   make_interpreter_init(&interpreter);
 
@@ -35,37 +103,6 @@ int main(int argc, char **argv) {
   if (err) {
     make_interpreter_free(&interpreter);
     return err;
-  }
-
-  options.filename = "Makefile";
-  options.working_dir = ".";
-  options.jobs = 1;
-
-  for (i = 1; i < argc; i++) {
-    if (strcmp(argv[i], "-C") == 0) {
-      options.working_dir = argv[i + 1];
-      if (options.filename == NULL) {
-        fprintf(stderr, "No directory given for '%s'\n", argv[i]);
-        return EXIT_FAILURE;
-      }
-      i++;
-    } else if (strcmp(argv[i], "-f") == 0) {
-      options.filename = argv[i + 1];
-      if (options.filename == NULL) {
-        fprintf(stderr, "No file name given for '%s'\n", argv[i]);
-        return EXIT_FAILURE;
-      }
-      i++;
-    } else if (strcmp(argv[i], "-j") == 0) {
-      if (sscanf(argv[i + 1], "%u", &options.jobs) != 0) {
-        fprintf(stderr, "Incorrect numerical value given for '%s'\n", argv[i]);
-        return EXIT_FAILURE;
-      }
-    } else {
-      /* The non-options will be
-       * parsed in the next loop */
-      break;
-    }
   }
 
   err = make_chdir(options.working_dir);
