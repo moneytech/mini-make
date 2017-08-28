433b90d6ad665ad5a0aa4d9cdb83c4cc969906fb
Taylor Holberton <taylorcholberton@gmail.com>
2017-08-28 17:44:12 -0400

    removed powershell invocation

--- job.c
+++ job.c
@@ -65,9 +65,6 @@ int make_job_start(struct make_job *job,
   struct make_string cmdline_copy;
 #if defined(__unix__)
   char *argv[4];
-#elif defined(_WIN32)
-  char powershell_data[] = "powershell -NoLogo -NonInteractive -Command ";
-  struct make_string powershell;
 #endif
 
   make_string_init(&cmdline_copy);
@@ -93,14 +90,6 @@ int make_job_start(struct make_job *job,
   exit(EXIT_FAILURE);
 #elif defined(_WIN32)
   err = make_string_copy(cmdline, &cmdline_copy);
-  if (err) {
-    make_string_free(&cmdline_copy);
-    return err;
-  }
-  powershell.data = powershell_data;
-  powershell.size = sizeof(powershell_data) - 1;
-  powershell.res = 0;
-  err = make_string_prepend(&cmdline_copy, &powershell);
   if (err) {
     make_string_free(&cmdline_copy);
     return err;
@@ -137,6 +126,8 @@ int make_job_wait(struct make_job *job, int *exit_code) {
 
 #if defined(__unix__)
   int status;
+#elif defined(_WIN32)
+  DWORD exit_code_2;
 #endif
 
 #if defined(__unix__)
@@ -151,10 +142,11 @@ int make_job_wait(struct make_job *job, int *exit_code) {
 #elif defined(_WIN32)
   WaitForSingleObject(job->process_info.hProcess, INFINITE);
   if (exit_code != NULL) {
-    if (!GetExitCodeProcess(job->process_info.hProcess, exit_code)) {
+    if (!GetExitCodeProcess(job->process_info.hProcess, &exit_code_2)) {
       /* TODO : get better error code */
       return -EINVAL;
     }
+    *exit_code = (int) exit_code_2;
   }
 #endif
 
