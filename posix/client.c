627231290c6f140a6ce374df67e4ccbaffdda60a
Taylor Holberton <taylorcholberton@gmail.com>
2017-10-11 17:41:18 -0400

    including string.h where missing

--- posix/client.c
+++ posix/client.c
@@ -2,9 +2,11 @@
 
 #include <mini-make/buffer.h>
 
-#include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+#include <unistd.h>
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <sys/un.h>
